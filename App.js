50670413fdd01599c30569ee3828f6ed16b3f605
TribuanaTunggaDewa <mujin.dev@gmail.com>
2019-10-08 23:18:15 +0700

    add 1.login_screen

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ class App extends Component {
     super(props)
       this.state = {
         showHide : 'eye-slash',
-        statusHide : false,
+        statusHide : true,
         noteEmail : '',
         notePassword : '',
         button_status : true
