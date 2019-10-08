8c126bd59d06f6cf74810796fb9f5e3a80e75e1f
TribuanaTunggaDewa <mujin.dev@gmail.com>
2019-10-08 17:40:53 +0700

    add-master

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
 import React, {Component} from 'react'
 import {View, Text, TextInput, StyleSheet, TouchableOpacity }  from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+      this.state = {
+        showHide : 'eye-slash',
+        statusHide : false,
+        inputEmail : '',
+        noteEmail : ''
+      }
+  }
+
+  validateEmail = (email) => {
+    
+    if(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email))
+    {
+      this.setState({ noteEmail: 'email yang anda masukkan sesuai format' })
+    }
+    else  {
+      this.setState({ noteEmail: 'Yang anda masukkan bukan email' })
+    }
+  }
 
   render(){
     return(
@@ -12,10 +34,16 @@ class App extends Component {
         </View>
         <View style={style.ContainerContent}>
           <Text style={style.Label,{marginTop:20}}>Email</Text>
-          <TextInput style={{ borderColor: 'black', borderWidth:2, height: 40}} />
-          <Text style={style.Label,{marginTop:20}}>Password</Text>
-          <TextInput style={{ borderColor: 'black', borderWidth:2, height: 40}} />
-          <TouchableOpacity style={{backgroundColor:'grey', borderWidth: 2, height: 35, marginTop: 20}}><Text style={{textAlign:"center", color:'white', fontSize: 18}}>Log IN</Text></TouchableOpacity>
+          <TextInput style={{ borderColor: 'black', borderWidth:2, height: 40}} onChangeText={(input_email)=>{this.setState({input_email}), this.validateEmail(input_email)}} value={this.state.input_email} />
+          <Text>{this.state.noteEmail}</Text>
+          <Text style={style.Label,{marginTop:10}}>Password</Text>
+          <View style={{flex:1,flexDirection:'row'}}>
+            <TextInput style={{ borderColor: 'black', borderWidth:2, height: 40, width:289}} secureTextEntry={this.state.statusHide} />
+            <TouchableOpacity style={{backgroundColor:'grey', height : 40, width : 40 }} onPress={()=> {if (this.state.showHide == 'eye-slash'){this.setState({showHide : 'eye', statusHide: true}) }else this.setState({showHide : 'eye-slash', statusHide: false}) }} >
+              <Icon name={this.state.showHide} size={30}  />
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity style={{backgroundColor:'grey', borderWidth: 2, height: 35, marginTop: 70}}><Text style={{textAlign:"center", color:'white', fontSize: 18}}>Log IN</Text></TouchableOpacity>
         </View>
       </View>
     )
