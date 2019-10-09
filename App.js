64e2b6270a3808635794629c325a69bf1f950030
TribuanaTunggaDewa <mujin.dev@gmail.com>
2019-10-09 08:56:03 +0700

    add 1.login_screen

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import {View, Text, TextInput, StyleSheet, TouchableOpacity }  from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
-import { creatStackNavigator, createAppContainer } from 'react-navigation'
 
 class HomeScreen extends Component {
 
